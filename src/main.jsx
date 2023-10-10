65cc7ab0f922771907c31416917080e1e0b17742
Thzzao <thiago.santos1160@gmail.com>
2023-10-10 17:01:31 -0300

    refactor: Eliminação de imports desnecessários

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './pages/App.jsx'
 import Rotas from './rotas.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
