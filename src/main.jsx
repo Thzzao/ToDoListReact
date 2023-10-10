0c89f86fd73cff822c9e32389af0a00038b057a5
Thzzao <thiago.santos1160@gmail.com>
2023-10-10 00:35:07 -0300

    feat: Importação das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './style/index.css'
+// import App from './pages/App.jsx'
+import Rotas from './rotas.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Rotas />
+    </React.StrictMode>,
 )
