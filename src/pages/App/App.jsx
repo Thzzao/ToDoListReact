0e82eae7c0aea1a2fee661ee53f195d07348721f
Thzzao <thiago.santos1160@gmail.com>
2023-10-10 17:08:08 -0300

    refactor: Separação das páginas em pastas próprias

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,21 @@
+import style from '../App/app.module.css'
+import { Header } from '../../components/Cabeçario/Header'
+import Pesquisa from '../../components/Pesquisa/Pesquisa'
+import Tarefa from '../../components/Tarefa/Tarefa'
+import Footer from '../../components/Rodapé/Footer'
+import Adicionar from '../../components/Adicionar/Adicionar'
+
+
+function App() {
+    return (
+        <div className={style.container}>
+            <Header />
+            <Adicionar />
+            <Pesquisa />
+            <Tarefa />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
