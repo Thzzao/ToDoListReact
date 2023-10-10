8eb4e8012b48d0ce74a514ae303a4a9c3dc856c3
Thzzao <thiago.santos1160@gmail.com>
2023-10-10 17:06:49 -0300

    feat: Criação do componente de renderizar as tarefas criadas

---
+++ src/components/Tarefa/Tarefa.jsx
@@ -0,0 +1,14 @@
+
+const Tarefa = () => {
+    return (
+
+        <div className='tarefa'>
+            {/* <label>{tarefa}</label> */}
+        </div>
+
+    )
+}
+
+// Criar cada tarefa com o mesmo layout
+
+export default Tarefa
