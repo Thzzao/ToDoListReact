0c8bba5a268c56330a36e806d2354f940a6e472a
Thzzao <thiago.santos1160@gmail.com>
2023-10-10 17:06:11 -0300

    feat: Criação do componente de rodapé

---
+++ src/components/Rodapé/Footer.jsx
@@ -0,0 +1,11 @@
+import style from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <div className={style.rodape}>
+            <footer>by Thzzao</footer>
+        </div>
+    )
+}
+
+export default Footer
