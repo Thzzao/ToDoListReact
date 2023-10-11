6944e804108295fe5c3a32ae0e1780e9d58f7d2d
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 00:46:07 -0300

    refactor: Mudança no botão para deixar ele customizavel

--- src/components/Botão/Button.jsx
+++ src/components/Botão/Button.jsx
@@ -1,15 +1,32 @@
 import "./Button.module.css"
 
-function Button({ click, text }) {
+// function Button({ click, text }) {
+//     return (
+//         <button onClick={(event) => {
+//             event.preventDefault()
+//             click()
+//         }}>{text}</button>
+//     )
+// }
+
+function Button({ text, click, color, border, height, width, backgroundColor, borderRadius }) {
+    const styleDefault = {
+        color: color || "#FFF",
+        border: border || "1px solid rgba(0, 0, 0, 0.10)",
+        height: height || "60px",
+        width: width || "350px",
+        backgroundColor: backgroundColor || " #4031EC",
+        borderRadius: borderRadius || "15px",
+        fontSize: "26px",
+
+    }
+
     return (
-        <button onClick={(event) => {
-            event.preventDefault()
-            click()
-        }}>{text}</button>
+        <button
+            style={styleDefault}
+            onClick={click}
+        >{text}</button>
     )
 }
 
-
 export default Button
-
-
