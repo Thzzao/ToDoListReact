83c90972e7cff21a6f70d6955641686cb2c3f46d
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 15:34:10 -0300

    fix: Arrumando importação

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Rotas from './rotas.jsx'
+import Rotas from './Rotas.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
