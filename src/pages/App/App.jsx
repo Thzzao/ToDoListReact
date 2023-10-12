6886a8e3b3819fb9c93a66d501bd4e28c7f6f533
Thzzao <thiago.santos1160@gmail.com>
2023-10-12 01:32:32 -0300

    refactor: Comentando alguns componentes para teste

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,17 +1,17 @@
 import style from '../App/app.module.css'
 import { Header } from '../../components/Cabeçario/Header'
-import Pesquisa from '../../components/Pesquisa/Pesquisa'
+// import Pesquisa from '../../components/Pesquisa/Pesquisa'
 import Tarefa from '../../components/Tarefa/Tarefa'
 // import Footer from '../../components/Rodapé/Footer'
-import Adicionar from '../../components/Adicionar/Adicionar'
+// import Adicionar from '../../components/Adicionar/Adicionar'
 
 
 function App() {
     return (
         <div className={style.container}>
             <Header />
-            <Adicionar />
-            <Pesquisa />
+            {/* <Adicionar /> */}
+            {/* <Pesquisa /> */}
             <Tarefa />
             {/* <Footer /> */}
         </div>
