dd37710c90b38cf67a33fb794afe452f9fed590a
Thzzao <thiago.santos1160@gmail.com>
2023-10-12 19:25:10 -0300

    refactor: Componentização das divisões e melhoria na condição de adicionar tarefa

--- src/components/Tarefa/Tarefa.jsx
+++ src/components/Tarefa/Tarefa.jsx
@@ -1,10 +1,7 @@
-/* eslint-disable react/prop-types */
-
 import { useState } from "react"
 import style from "../Tarefa/Tarefa.module.css"
-import Button from "../Botão/Button"
-import line from "../../assets/Line 1.svg"
-import Pesquisa from "../Pesquisa/Pesquisa"
+import Adicionar from "../Adicionar/Adicionar"
+import BoxTarefa from "../BoxTarefa/BoxTarefa"
 
 const Tarefa = () => {
     const [value, setValue] = useState("")
@@ -35,66 +32,43 @@ const Tarefa = () => {
     ])
 
     const addTarefa = (text) => {
-        const newTarefa = [...tarefas,
-        {
-            id: Math.floor(Math.random() * 10000),
-            text: text,
-        },
-        ]
-        setTarefas(newTarefa)
-    }
-
-    function excluirTarefa(id) {
-        setTarefas(tarefas.filter(tarefa => tarefa.id !== id))
-    }
-
-    function editarTarefa() {
-        // setValue(e.target.value)
+        if (text.length < 3) {
+            return
+        } else {
+            const newTarefa = [...tarefas,
+            {
+                id: Math.floor(Math.random() * 10000),
+                text: text,
+            },
+            ]
+            setTarefas(newTarefa)
+        }
     }
 
     const [pesquisa, setPesquisa] = useState("")
 
+    const excluirTarefa = (id) => {
+        setTarefas(tarefas.filter(tarefa => tarefa.id !== id))
+    }
+
+    const editarTarefa = () => {
+        //fazer a edição usando o estado (se true return um input)
+
+
+    }
+
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div className={style.divtarefa} >
-                    <input className={style.tarefa} type="text" placeholder='Nome da tarefa' value={value} onChange={(e) => setValue(e.target.value)}></input>
+            <Adicionar handleSubmit={handleSubmit} value={value} setValue={setValue} addTarefa={addTarefa} pesquisa={pesquisa} setPesquisa={setPesquisa} />
 
-                    <Button type="submit" text='+ Adicionar' width='250px' height='45px' borderRadius='7px' click={() => {
-                        addTarefa(value)
-                    }}></Button>
-                </div>
-
-                <div >
-                    <img className={style.linha} src={line} alt='Linha azul separando o título do botão'></img>
-                </div>
-
-                <Pesquisa pesquisa={pesquisa} setPesquisa={setPesquisa} />
-
-            </form>
             <div className={style.listagem}>
                 {
                     tarefas.filter((tarefa) => tarefa.text.toLowerCase().includes(pesquisa.toLowerCase())).map((tarefa) => (
-                        <div key={tarefa.id} className={style.boxtarefa} >
-                            <div className={style.box}>
-                                <label><input type="checkbox" className={style.checkbox} /><span>{tarefa.text}</span></label>
-
-                                <button className={style.excluir} onClick={() => {
-                                    excluirTarefa(tarefa.id)
-                                }}><img src="\src\assets\bin.svg"></img>
-                                </button>
-                            </div>
-
-                            <button className={style.editar} onClick={() => {
-
-                                editarTarefa()
-
-                            }} ><img src="\src\assets\cil_pen.svg"></img></button>
-                        </div>
+                        <BoxTarefa key={tarefa.id} excluirTarefa={excluirTarefa} editarTarefa={editarTarefa} text={tarefa.text} id={tarefa.id} />
                     ))}
             </div>
         </>
-
     )
 }
+
 export default Tarefa
