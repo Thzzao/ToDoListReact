5aa901b8416b7ee40361cb55762665619c781a57
Thzzao <thiago.santos1160@gmail.com>
2023-10-12 19:19:00 -0300

    feat: Criação do componente de divisão de cada tarefa

---
+++ src/components/BoxTarefa/BoxTarefa.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import style from '../BoxTarefa/boxTarefa.module.css'
+
+const BoxTarefa = ({ excluirTarefa, editarTarefa, text, id }) => {
+
+    const [concluido, setConcluido] = useState(false)
+
+    return (
+        <div key={id} className={style.boxtarefa} >
+            <div className={`${style.box} ${!concluido ? style.ativo : style.desativado} `}>
+                <label><input type="checkbox" onChange={() => (setConcluido(!concluido))} value={concluido} className={style.checkbox} /><span>{text}</span></label>
+
+                <button className={style.excluir} onClick={() => {
+                    excluirTarefa(id)
+                }}><img src="\src\assets\bin.svg"></img>
+                </button>
+            </div>
+
+            <button className={style.editar} onClick={() => {
+                editarTarefa()
+            }} ><img src="\src\assets\cil_pen.svg"></img></button>
+        </div>
+    )
+}
+
+export default BoxTarefa
