f39bd3485da48bbbb821525f14dc60620b8b3220
Thzzao <thiago.santos1160@gmail.com>
2023-10-12 19:27:20 -0300

    feat: Criação de uma div para arrumar o footer da página

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
-import style from '../Home/home.module.css'
 import { Header } from '../../components/Cabeçario/Header'
 import { useNavigate } from 'react-router-dom'
+import style from '../Home/home.module.css'
 import Button from "../../components/Botão/Button"
 import line from "../../assets/Line 1.svg"
 import Footer from '../../components/Rodapé/Footer'
@@ -12,17 +12,20 @@ const Home = () => {
     }
     return (
         <div className={style.container}>
-            <Header />
-            <div className={style.linha}>
-                <img src={line} alt='Linha azul separando o título do botão'></img>
-            </div>
-            <div className={style.botao}>
-                <Button text="START" click={() => {
-                    handleClick()
-                }}></Button>
+            <div className={style.content}>
+                <Header />
+                <div className={style.linha}>
+                    <img src={line} alt='Linha azul separando o título do botão'></img>
+                </div>
+                <div className={style.botao}>
+                    <Button text="START" click={() => {
+                        handleClick()
+                    }}></Button>
+                </div>
             </div>
             <Footer />
         </div>
     )
 }
+
 export default Home
