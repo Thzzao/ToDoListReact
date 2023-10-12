75a36af8629d378804640d52f6f5587515827cd6
Thzzao <thiago.santos1160@gmail.com>
2023-10-12 01:31:56 -0300

    feat: Junção do componente 'adicionar' no componente das tarefas

--- src/components/Tarefa/Tarefa.jsx
+++ src/components/Tarefa/Tarefa.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/prop-types */
-// import { useState } from "react"
+
 import { useState } from "react"
 import style from "../Tarefa/Tarefa.module.css"
-// import Adicionar from "../Adicionar/Adicionar"
-
+import Button from "../Botão/Button"
+import line from "../../assets/Line 1.svg"
+import Pesquisa from "../Pesquisa/Pesquisa"
 
 const Tarefa = () => {
+    const [value, setValue] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!value) return;
+        setValue("")
+    }
+
     const [tarefas, setTarefas] = useState([
         {
             id: 1,
@@ -29,37 +38,59 @@ const Tarefa = () => {
         },
     ])
 
+    const addTarefa = (text) => {
+        const newTarefa = [...tarefas,
+        {
+            id: Math.floor(Math.random() * 10000),
+            text: text,
+            isCompleted: false,
+        },
+        ]
+        setTarefas(newTarefa)
+    }
+
+    function excluirTarefa(id) {
+        setTarefas(tarefas.filter(tarefa => tarefa.id !== id))
+    }
 
     return (
         <>
-            <div className={style.listagem}>
-                {tarefas.map((tarefa) => (
-                    <div key={tarefa.id} className="tarefa">
-                        <div className="content">
-                            <p>{tarefa.text}</p>
-                        </div>
-                    </div>
-                    // 
-                ))}
-                <div>
-                    <img className={style.excluir} onClick="" src="\src\assets\bin.svg"></img>
-                    <img className={style.editar} onClick="" src="\src\assets\cil_pen.svg"></img>
-                    <p><label><input type="checkbox" className={style.checkbox} /><span>XXXXXXX</span></label></p>
+            <form onSubmit={handleSubmit}>
+                <div className={style.divtarefa} >
+                    <input className={style.tarefa} type="text" placeholder='Nome da tarefa' value={value} onChange={(e) => setValue(e.target.value)}></input>
+
+                    <Button type="submit" text='+ Adicionar' width='250px' height='45px' borderRadius='7px' click={() => {
+                        addTarefa(value)
+                    }}></Button>
                 </div>
+
+                <div >
+                    <img className={style.linha} src={line} alt='Linha azul separando o título do botão'></img>
+                </div>
+
+                <Pesquisa />
+
+            </form>
+            <div className={style.listagem}>
+                {
+                    tarefas.map((tarefa) => (
+                        <div key={tarefa.id} className={style.boxtarefa} >
+                            <div className={style.box}>
+                                <label><input type="checkbox" className={style.checkbox} /><span>{tarefa.text}</span>
+                                </label>
+
+                                <button className={style.excluir} onClick={() => {
+                                    excluirTarefa(tarefa.id)
+                                }}><img onClick="" src="\src\assets\bin.svg"></img>
+                                </button>
+                            </div>
+
+                            <button className={style.editar}><img onClick="" src="\src\assets\cil_pen.svg"></img></button>
+                        </div>
+                    ))}
             </div>
         </>
 
     )
 }
-
-
-
-//<div className={style.tarefa}>
-{/* <label>{tarefa}</label> */ }
-//    <label>TAREFAAA</label>
-//</div>
-
-
-// Criar cada tarefa com o mesmo layout
-
 export default Tarefa
