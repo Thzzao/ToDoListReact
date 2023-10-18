deacecb91b86c08ffa185df06e1fc0493323c552
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 00:20:25 -0300

    refactor: Remoção do fragment

--- src/components/Editar/Editar.jsx
+++ src/components/Editar/Editar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
-import style from "../Editar/editar.module.css"
 import Button from "../Botão/Button"
-
+import style from "../Editar/editar.module.css"
 
 export const Editar = ({ editarTarefa, task }) => {
     const [value, setValue] = useState(task.text)
@@ -13,16 +12,13 @@ export const Editar = ({ editarTarefa, task }) => {
     }
 
     return (
+        <form onSubmit={handleSubmit} className={style.divtarefa} >
+            <input className={style.tarefa} type="text" placeholder='Nome da nova tarefa' value={value} onChange={(e) => setValue(e.target.value)}></input>
 
-        <>
-            <form onSubmit={handleSubmit} className={style.divtarefa} >
-                <input className={style.tarefa} type="text" placeholder='Nome da nova tarefa' value={value} onChange={(e) => setValue(e.target.value)}></input>
-
-                <Button type="submit" text='Atualizar' width='200px' height='45px' borderRadius='7px' click={() => {
-                    editarTarefa(value)
-                }}></Button>
-            </form>
-        </>
+            <Button type="submit" text='Atualizar' width='200px' height='45px' borderRadius='7px' click={() => {
+                editarTarefa(value)
+            }}></Button>
+        </form>
     )
 }
 
