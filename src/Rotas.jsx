36e7a51d16f5127597374373a38bae9e648b8d01
Thzzao <thiago.santos1160@gmail.com>
2023-10-30 00:40:58 -0300

    feat: Importando a estilização global

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import App from './pages/App/App'
+import { GlobalStyle } from './GlobalStyle'
 
 const Rotas = () => {
     return (
         <BrowserRouter>
+            <GlobalStyle />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/app' element={<App />} />
